d3ad8ee3bde4fdca6a39368feafc7b36b3dc8f8b
DmitriyBarinov <95102030+Defolt023@users.noreply.github.com>
2022-03-24 22:15:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,8 +1,32 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
+  k = 0;
+  for (int i = 0; i < len; i++) {
+  for (int j = 0; i < len; j++) {
+  if arr [i] + arr[j] == value) {
+  k++;
+  }
+  }
+  }
+  if (k > 0) {
+  return k;
+  } else {
   return 0;
 }
 int countPairs2(int *arr, int len, int value) {
+  while (arr[len - 1] > value) {
+  len--;
+  k = 0;
+  for (int i = 0; i < len; i++) {
+  for (int j = 0; i < len; j++) {
+  if arr [i] + arr[j] == value) {
+  k++;
+  }
+  }
+  }
+  if (k > 0) {
+  return k;
+  } else {
   return 0;
 }
 int countPairs3(int *arr, int len, int value) {
