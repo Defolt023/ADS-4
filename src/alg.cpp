7c0ba4a9d75e91e4a7915a1c39600a38580632ec
DmitriyBarinov <95102030+Defolt023@users.noreply.github.com>
2022-04-22 21:13:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,11 +1,11 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int arr, int len, int value) {
-  int j = 0;
   int i = 0;
-  int kol = 0;
-  for (int j = 0; j < len; j++) {
-  for (int i = j + 1; i < len; i++) {
-  if (arr[j] + arr[i] == value) {
+  int j = 0;
+  int k = 0;
+  for (int i = 0; i < len; i++) {
+  for (int j = i + 1; j < len; j++) {
+  if (arr[i] + arr[j] == value) {
   k = k + 1;
   }
   }
@@ -16,12 +16,12 @@ int countPairs2(intarr, int len, int value) {
   while (arr[len - 1] > value) {
   len = len - 1;
   }
-  int j = 0;
   int i = 0;
-  int k = 0;
-  for (int j = 0; j < len; j++) {
-  for (int i = j + 1; i < len; i++) {
-  if (arr[j] + arr[i] == value) {
+  int j = 0;
+  int kol = 0;
+  for (int i = 0; i < len; i++) {
+  for (int j = i + 1; j < len; j++) {
+  if (arr[i] + arr[j] == value) {
   k = k + 1;
   }
   }
@@ -30,18 +30,18 @@ int countPairs2(intarr, int len, int value) {
 }
 int countPairs3(int *arr, int len, int value) {
   int k = 0;
-  for (int j = 0; j < len; j++) {
-  int q = j + 1;
-  int b = len - 1;
-  while (q < b) {
-  int s = (q + b) / 2;
-  if (arr[s] + arr[j] < value) {
+  for (int i = 0; i < len; i++) {
+  int q = i + 1;
+  int d = len - 1;
+  while (q < d) {
+  int s = (q + d) / 2;
+  if (arr[s] + arr[i] < value) {
   q = s + 1;
   } else {
-  b = s;
+  d = s;
   }
   }
-  while (arr[q] + arr[j] == value) {
+  while (arr[q] + arr[i] == value) {
   k += 1;
   q += 1;
   }
